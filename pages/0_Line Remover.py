7a1866d4c8ddfa3e710863b56b0023e101146a20
Farhan Masood <farhan.mk21@gmail.com>
2024-04-03 05:48:16 -0500

    Added some color

--- pages/0_Line Remover.py
+++ pages/0_Line Remover.py
@@ -16,7 +16,7 @@ st.sidebar.success("Select a tool above.")
 col1,col2 = st.columns(2)
 
 with col1:
-    t1 = st.text_area(label = 'Please enter any text to remove extra lines from:',placeholder = 'Input text here',height = 500)
+    t1 = st.text_area(label = ':red[Please enter any text to remove extra lines from:]',placeholder = 'Input text here',height = 500)
     len1 = len(t1)
     tok1 = len1/4
     st.text(f"{len1} characters (~{math.ceil(tok1)} tokens)")
@@ -25,7 +25,7 @@ According to OpenAI:
 1 token = ~4 characters''')
 
 with col2:
-    st.text_area(label = 'Here is your input with extra lines removed:',placeholder = 'Output text here',value = "".join([s for s in t1.splitlines(True) if s.strip('\n')]),height = 500)
+    st.text_area(label = ':green[Here is your input with extra lines removed:]',placeholder = 'Output text here',value = "".join([s for s in t1.splitlines(True) if s.strip('\n')]),height = 500)
     t2 = "".join([s for s in t1.splitlines(True) if s.strip('\n')])
     len2 = len(t2)
     tok2 = len2/4
