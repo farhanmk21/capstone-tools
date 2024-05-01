4d6c8cd7ad60819f25b270274e4549e7dbf7b406
Farhan Masood <farhan.mk21@gmail.com>
2024-05-01 02:33:57 -0500

    Changed home title

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 st.markdown("""<style>body {zoom: 1.1;  /* Adjust this value as needed */}</style>""", unsafe_allow_html=True)
 
-st.title("Welcome to Capstone Tools")
+st.title("Welcome to Capstone Tools 🫡")
 
 st.header("This is an online app created to have all the tools we use for our capstone in one spot.")
 st.sidebar.success("Select a tool above.")
